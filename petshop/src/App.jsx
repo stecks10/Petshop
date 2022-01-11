941993d6b374a22818550e3f888145ae8f507141
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-01-11 13:38:55 -0300

    Erro 404

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './assets/css/base/base.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
 
 function App() {
-  const Router = () => {
-    const location = window.location.pathname;
-    if (location === '/sobre') {
-      return <Sobre />;
-    } else {
-      return <Home />;
-    }
-  };
-  return <>{Router()}</>;
+  return (
+    <>
+      <Home />
+      <Sobre />
+      <Pagina404 />
+    </>
+  );
 }
 
 export default App;
