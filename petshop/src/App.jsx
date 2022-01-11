2186d21a9d534874302d59ae1dd1e6a99d2e663f
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-01-11 16:10:42 -0300

    Criando NavBar

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -5,10 +5,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
 import Pagina404 from './paginas/Pagina404';
+import Cabecalho from './components/Cabecalho';
 
 function App() {
   return (
     <>
+      <Cabecalho />
       <Home />
       <Sobre />
       <Pagina404 />
